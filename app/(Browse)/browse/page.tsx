c7a3ec2c4a191076e8d3530269384b3116e5ee65
Hazem Gamal <hazemgamaltaha3@yahoo.com>
2024-01-13 13:45:27 +0200

    code fixes

--- app/(Browse)/browse/page.tsx
+++ app/(Browse)/browse/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import { FaFilter } from "react-icons/fa";
 
 const page = () => {
   return (
     <div>
       <Navbar />
+      <div className='w-full'>
+        <div className='max-w-screen-2xl mx-auto py-4 border-b-2 px-4 2xl:px-0'>
+          <div>
+            <p className='text-lg flex gap-2 items-center cursor-pointer'><FaFilter className="text-lg"/>Filter</p>
+          </div>
+        </div>
+        <div className='grid place-content-center h-screen w-full'>
+          <h1 className='font-bold'>Books grid</h1>
+        </div>
+
+      </div>
     </div>
   )
 }
